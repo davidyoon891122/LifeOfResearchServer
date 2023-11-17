6c42fb17bda3b43d3d4cbafdb0d2d345b06b1019
David Yoon <qqpo12@naver.com>
2023-11-17 15:33:06 +0900

    Add a TypeOrmModule
    * Added a typeOrmModule and a postgres module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NewModule } from './new/new.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
 
 @Module({
-  imports: [NewModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      synchronize: true,
+    }),
+    NewModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
