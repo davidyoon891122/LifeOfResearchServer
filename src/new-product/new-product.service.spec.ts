bab8923705d95f8ddfefde5d35deb26690bdfaee
David Yoon <qqpo12@naver.com>
2023-11-20 16:31:00 +0900

    Add a test case
    * Added a new-product.service's test cases with mock data

--- src/new-product/new-product.service.spec.ts
+++ src/new-product/new-product.service.spec.ts
@@ -1,12 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NewProductService } from './new-product.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { NewProduct } from './entity/new-product.entity';
+import { Repository } from 'typeorm';
+import { CreateNewProductDto } from './dto/create-newProduct.dto';
 
 describe('NewProductService', () => {
   let service: NewProductService;
+  const mockProduct: NewProduct = {
+    id: 1,
+    productId: '0001',
+    name: 'AIR FORCE1',
+    category: 'NEW',
+    imagePath: 'localhost:3000/resource/AIRFOCE1',
+    brand: 'NIKE',
+    description: 'This product is the famouse shose in the world',
+    price: 120000,
+    isLike: false,
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [NewProductService],
+      providers: [
+        {
+          provide: NewProductService,
+          useValue: {
+            getAllProduct: jest.fn(() => [mockProduct]),
+            getProductById: jest.fn(() => ({})),
+            createNewProduct: jest.fn(() => ({})),
+            deleteNewProductById: jest.fn(() => ({})),
+            updateNewProduct: jest.fn(() => ({})),
+          },
+        },
+        {
+          provide: getRepositoryToken(NewProduct),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     service = module.get<NewProductService>(NewProductService);
@@ -14,5 +44,13 @@ describe('NewProductService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getAllProduct', () => {
+    it('should be returned All Product', async () => {
+      const result = await service.getAllProduct();
+      console.log(result);
+      expect(result.length).toEqual(1);
+    });
   });
 });
