a3edbbd506f2b596a9f4742ce476bda89c31ec6a
David Yoon <qqpo12@naver.com>
2023-11-24 13:38:50 +0900

    Add a main.hbs
    * Added a main.hbs to upload images

--- src/admin/login/admin.login.controller.ts
+++ src/admin/login/admin.login.controller.ts
@@ -1,20 +1,24 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { AdminLoginService } from './admin.login.service';
 import { SearchAdminUserDto } from './dto/search-adminUser.dto';
+import { Response } from 'express';
 
 @Controller('admin/login')
 export class AdminLoginController {
   constructor(private readonly adminLoginService: AdminLoginService) {}
   @Post()
-  async loginProcess(@Body() userLoginInfo: SearchAdminUserDto) {
+  async loginProcess(
+    @Res() res: Response,
+    @Body() userLoginInfo: SearchAdminUserDto,
+  ) {
     console.log(userLoginInfo);
     try {
       const result = await this.adminLoginService.checkLoginUser(userLoginInfo);
 
       if (result) {
-        return 'Go to admin main page';
+        return res.render('main');
       } else {
-        return 'Redirect to login page with error popup';
+        return res.render('index', { message: 'Password Error' });
       }
     } catch (error) {
       return 'Not Existed User';
